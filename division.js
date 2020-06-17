001aa7c8f8ed9be33146ea236d60b21794742a39
GurmehrBindra <64597931+GurmehrBindra@users.noreply.github.com>
2020-06-17 13:29:58 +0530

    Update division.js

--- division.js
+++ division.js
@@ -1,6 +1,9 @@
 class Division{
     constructor(x,y,width,height){
-        this.body= Bodies.rectangle(x,y,width,height);
+        var options={
+            "isStatic":true
+        }
+        this.body= Bodies.rectangle(x,y,width,height,options);
         this.width=width;
         this.height=height;
         World.add(world,this.body);
